ffdfe2653e020283c43fa0ed0ec6ff0ca06f4b28
Alex Reich <atrain.reich@gmail.com>
2018-06-05 16:17:21 -0600

    Need to finish/fix routes

--- server/server-assets/routes/Owners.js
+++ server/server-assets/routes/Owners.js
@@ -2,25 +2,36 @@ var router = require('express').Router()
 var Owners = require('../models/owner')
 var session = require('../../auth/session')
 
-// Get Trucks
-router.get('/api/owners/trucks/:id?', (req, res) => {
-  if (req.params.id) {
-    Owners.findById(req.params.id)
-      .then(playlist => {
-        return res.status(200).send(playlist)
-      })
-      .catch(err => {
-        res.status(400).send(err)
-      })
-  }
-}
-// Create New Truck
+// Get All Trucks
+router.get('/api/owners/trucks', (req, res) => {
+  Owners.find(req.query)
+  .then(owners=>{
+    res.status(200).send(owners)
+  })
+  .catch(err=>{
+    res.status(400).send(err)
+  })
+})
+
+// Add a truck to an owner
+router.put('/api/owners/:id/trucks', (req,res)=>{
+  Owners.findById(req.params.id)
+    .then(function(playlist){
+      playlist.songs.addToSet(req.body)
+      playlist.save()
+        .then(()=>{
+          console.log("Successfully Updated ", playlist)
+          res.status(200).send(playlist)
+        })
+    })
+    .catch(err=>{
+      res.status(400).send(err)
+    })
+})
 
 // Edit Truck
 
 // Delete Truck
 
 
-module.exports = {
-    router
-  }
+module.exports = { router }

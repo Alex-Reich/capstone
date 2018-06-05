dd3dd31f8a14d1c0467115cd8e36fa559825564d
Christian Tran <christiantran04@gmail.com>
2018-06-05 14:26:09 -0600

    ct modals ready for routes

--- www/src/components/Home.vue
+++ www/src/components/Home.vue
@@ -1,15 +1,11 @@
 <template>
+<div>
   <div class="home container-fluid">
     <div class="main-top-style">
 
-<<<<<<< HEAD
-<button type="button" class='btn btn-link' data-toggle="modal" data-target="#createLoginModal">Login</button>
-          <!-- Modal -->
-=======
-<!-- <button type="button" class='btn btn-link' data-toggle="modal" data-target="#createLoginModal">Login</button>
-          Modal -->
->>>>>>> aaba149532541943cdea025e0d21b37e13ea93b6
-          <!-- <div class="modal fade" id="createLoginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <!-- Login Modal -->
+          <button type="button" class='btn btn-primary' data-toggle="modal" data-target="#loginModal">Login</button>
+          <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
               <div class="modal-content">
                 <div class="modal-header">
@@ -21,25 +17,57 @@
                 <div class="modal-body">
                   <form v-on:submit.prevent="login">
                     <div class="form-group">
-                      <input type="text" name="loginUsername" v-model="
-                          user.username" class="form-control" id="formGroupExampleInput" placeholder="Username" required>
+                      <input type="text" name="ownerUsername" v-model="
+                          login.username" class="form-control" id="formGroupExampleInput" placeholder="Username" required>
                     </div>
                     <div class="form-group">
-                      <input type="text" name="Password" v-model="
-                              user.password" class="form-control" id="formGroupExampleInput" placeholder="Password">
+                      <input type="text" name="password" v-model="
+                              login.password" class="form-control" id="formGroupExampleInput" placeholder="Password">
                     </div>
                   </form>
                 </div>
                 <div class="modal-footer">
+                  <button type="button" @click="login" class="btn btn-primary" data-dismiss="modal">Login</button>
                   <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                  <button type="button" @click="login" class="btn btn-primary" data-dismiss="modal">Submit</button>
                 </div>
               </div>
             </div>
-          </div> -->
+          </div>
 
-          <button type="submit">Register</button>
+          <!-- Register Modal -->
+          <button type="button" class='btn btn-primary' data-toggle="modal" data-target="#registerModal">Register</button>
+          <div class="modal fade" id="registerModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Register</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <form v-on:submit.prevent="register">
+                    <div class="form-group">
+                      <input type="text" name="ownerRegister" v-model="register.username" class="form-control" id="formGroupExampleInput" placeholder="Username" required>
+                    </div>
+                    <div class="form-group">
+                      <input type="text" name="email" v-model="register.email" class="form-control" id="formGroupExampleInput" placeholder="email">
+                    </div>
+                    <div class="form-group">
+                      <input type="text" name="password" v-model="register.password" class="form-control" id="formGroupExampleInput" placeholder="Password">
+                    </div>
+                  </form>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" @click="login" class="btn btn-primary" data-dismiss="modal">Login</button>
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
       <h1>Trucking Hangry</h1>
+
       <img src="http://placehold.it/150x150">
     </div>
     <div class="main-bottom-style">
@@ -51,16 +79,56 @@
       </form>
     </div>
   </div>
+  </div>
 </template>
 
+
+
 <script>
+// import router from './router'
+// import owner from '../owner'
+
   export default {
     name: 'Home',
     data() {
       return {
+        login: {
+          username: '',
+           password: ''
+        },
+        register: {
+          username: '',
+          email: '',
+          password: '',
+        }
+        }
+      },
+    computed: {
+
+    },
+    methods: {
+      ownerLogin() {
+        this.$store.dispatch()
+        this.register = {
+          email: '',
+          password: ''
+        }
+      },
+      ownerRegister() {
+        this.$store.dispatch()
+        this.register = {
+          name: '',
+          email: '',
+          password: ''
+        }
 
       }
-    }
+    },
+
+
+
+
+    
   }
 </script>
 
